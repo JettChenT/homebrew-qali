532e24418efbfc7c99edd7d0d71b054f25b70950
JettChenT <jettchen12345@gmail.com>
2022-08-29 22:36:15 +0800

    update version

--- Formula/qali.rb
+++ Formula/qali.rb
@@ -2,8 +2,8 @@ class Qali < Formula
     desc "Quick ALIasing in the terminal"
     homepage "https://github.com/JettChenT/qali"
     url "https://github.com/JettChenT/qali/releases/latest/download/qali-x86_64-apple-darwin.tar.gz"
-    sha256 "9eb6c5e5f1cd4f80dd95f78b1123e973c190445e3d4dcd431ac124fd16f8d5c4"
-    version "0.3.4"
+    sha256 "4cb86aa63b138f8531f84ce1a4f2ff6af76642b586e7314c9999abae70347c2a"
+    version "0.3.5"
 
     def install
         bin.install "qali"
